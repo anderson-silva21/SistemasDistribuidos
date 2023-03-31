d459460a6880f66856b257cd3dcb69a6b2ac3117
Anderson Silva <andsoares2001@gmail.com>
2023-03-31 20:38:47 -0300

    Delete FirstProject directory

--- FirstProject/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        var printer = new Printer();
-        System.out.println(printer.toString());
-    }
-}
--- FirstProject/src/Printer.java
+++
@@ -1,5 +0,0 @@
-public class Printer {
-    public Printer(){
-        System.out.println("Eu sou a classe printer");
-    }
-}
